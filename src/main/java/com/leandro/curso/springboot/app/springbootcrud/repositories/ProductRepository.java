6bd4b819d3e39bd111ee9f85e724890d8ef4a627
Leandro-Caponetto <caponettopeppers@gmail.com>
2024-02-15 12:46:18 -0300

    JWT- Authorization

---
+++ src/main/java/com/leandro/curso/springboot/app/springbootcrud/repositories/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.leandro.curso.springboot.app.springbootcrud.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.leandro.curso.springboot.app.springbootcrud.entities.Product;
+
+public interface ProductRepository extends CrudRepository<Product, Long> {
+    boolean existsBySku(String sku);
+}
---
+++ src/main/java/com/leandro/curso/springboot/app/springbootcrud/services/UserService.java
@@ -0,0 +1,14 @@
+package com.leandro.curso.springboot.app.springbootcrud.services;
+
+import java.util.List;
+
+import com.leandro.curso.springboot.app.springbootcrud.entities.User;
+
+public interface UserService {
+
+    List<User> findAll();
+
+    User save(User user);
+
+    boolean existsByUsername(String username);
+}
